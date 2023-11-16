d89dad03b3541a1d9ead9a924c360c82f1edf568
naresh-sthr <inbookslimx2@gmail.com>
2023-11-16 21:10:00 +0530

    Keypress on Enter added

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 var url="https://api.qrserver.com/v1/create-qr-code/?size=170x170&data=";
 let InputValue = document.getElementById('Inputin');
 let imagesrc = document.getElementById('qr-image');
-
 function generate() {
+    
     imagesrc.src=url+InputValue.value;
+
 }
+
+InputValue.addEventListener("keypress",function(event){
+    if(event.key=="Enter"){
+        generate();
+    }
+})
